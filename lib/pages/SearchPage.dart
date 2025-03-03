a688fe07dd574794d03840c96e07897ba6ff7f43
Chayutpong Phonyiam <6510525022@student.tu.ac.th>
2025-03-04 00:03:30 +0700

    add navigate and pages file

---
+++ lib/pages/SearchPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Search Page',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
